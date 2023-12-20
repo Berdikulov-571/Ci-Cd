5d0e99ff5f7659e05d75cfb7f84a1456231fe216
berdikulov-571 <mygithub571@gmail.com>
2023-12-20 15:40:13 +0500

    Math Controller

---
+++ Math.Api/Controllers/MathController.cs
@@ -0,0 +1,18 @@
+﻿using Math.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Math.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class MathController : ControllerBase
+    {
+        [HttpGet]
+        public async ValueTask<IActionResult> AddAsync([FromQuery] long a, [FromQuery] long b, [FromServices] IMathService service, CancellationToken cancellationToken = default)
+        {
+            var result = await service.AddAsync(a, b,cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
