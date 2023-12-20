8f4a2c98b6685922e5c005047c9876780b487561
berdikulov-571 <mygithub571@gmail.com>
2023-12-20 15:40:33 +0500

    MathService

---
+++ Math.Api/Services/MathService.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Math.Api.Services
+{
+    public class MathService : IMathService
+    {
+        public Task<long> AddAsync(long a, long b, CancellationToken cancellationToken = default)
+        {
+            var res = checked(a + b);
+            return Task.FromResult(res);
+        }
+    }
+}
