baa5000da92f3f1d15e5f1e8f1fb20321a58e20e
berdikulov-571 <mygithub571@gmail.com>
2023-12-20 15:41:01 +0500

    Math Service

---
+++ Math.Test/Services/MathServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Math.Api.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Math.Test.Services
+{
+    public class MathServiceTest
+    {
+        private readonly ServiceProvider _provider;
+        public MathServiceTest()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddScoped<IMathService, MathService>();
+            _provider = serviceCollection.BuildServiceProvider();
+        }
+
+        [Fact]
+        public async void ShouldAddTwoNumberCorrectly()
+        {
+            long a = 10;
+            long b = 20;
+            var mathService = _provider.GetRequiredService<IMathService>();
+            long expectedResult = 30;
+
+            var result = await mathService.AddAsync(a, b);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
